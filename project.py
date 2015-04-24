f3d474a9e905264efa2071008e927c80c7c34752
Ignacio Zuniga <Izuniga485@gmail.com>
2015-04-24 12:13:14 -0700

    Update project.py

--- project.py
+++ project.py
@@ -1,32 +1,26 @@
-'''
-Created on Apr 23, 2015
-
-@author: Deadsec
-
-http://www.themeta.gg
-
-This module makes an API call to a game and stores a json object to a textfile.
-The JSON object is statistics on a game's relevant meta information.
-
-'''
 import urllib2
 import json
+import re
 
 def main():
-    response = urllib2.urlopen('https://na.api.pvp.net/observer-mode/rest/' +
-                'featured?api_key=e63ca19d-7ce7-4fc7-9b85-35759aab7ec6')
+    response = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/35241849?api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
     string = response.read()        #this is a string
     gameList = json.loads(string)   #this is a json object
-    
-    
-    #print(string)
-    print(gameList)     #the u' annotation is unicode.  you can ignore it
-    
-    #prints out the values stored in the 'gameList' key, this will help us determine what
-    #we can use to answer our question, it is stored as a dictionary
-    for i in gameList['gameList']:
-        print(i)              
-    
+   
+    c = string.count('matchId')
+
+    #theChamps = []
+
+    for i in range(0,c):
+        print gameList["matches"][i]["matchId"] #the u' annotation is unicode.  you can ignore it
+        print gameList["matches"][i]["participants"][0]["championId"] #the u' annotation is unicode.  you can ignore it
+        #print gameList["matches"][i]["ParticipantStats"][0]["winner"]  #this also does not work 
+        #heChapms.append(gameList["matches"][i]["ParticipantStats"][0]["winner"]) # this shit does not work
+
+    #for i in range(0, len(theChamps)):
+           # response = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/35241849?api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
+
+
 
 if __name__ == '__main__':
     main()
