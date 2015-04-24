54381955a84a9c8958d88fef14e9823ec2230468
Ignacio Zuniga <Izuniga485@gmail.com>
2015-04-24 14:24:42 -0700

    Update project.py
    accidentally erased all of hugos code which made it look nice

--- project.py
+++ project.py
@@ -17,18 +17,12 @@ def main():
         print champion["data"][name]["name"], "=", champion["data"][name]["id"]
 
 
-    #theChamps = []
-
+    
+    print("===Game Data Sample===") 
     for i in range(0,c):
-        print gameList["matches"][i]["matchId"] #the u' annotation is unicode.  you can ignore it
-        print gameList["matches"][i]["participants"][0]["championId"] #the u' annotation is unicode.  you can ignore it
-        print gameList["matches"][i]["participants"][0]["stats"]["winner"] #the u' annotation is unicode.  you can ignore it
-
-        #print gameList["matches"][i]["ParticipantStats"][0]["winner"]
-        #heChapms.append(gameList["matches"][i]["ParticipantStats"][0]["winner"])
-
-    #for i in range(0, len(theChamps)):
-           # response = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/35241849?api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
+        print "Match ID = ", gameList["matches"][i]["matchId"] #the u' annotation is unicode.  you can ignore it
+        print "Champion ID = ", gameList["matches"][i]["participants"][0]["championId"] #the u' annotation is unicode.  you can ignore it
+        print "Win = ", gameList["matches"][i]["participants"][0]["stats"]["winner"], "\n" #the u' annotation is unicode.  you can ignore it
 
 
 
