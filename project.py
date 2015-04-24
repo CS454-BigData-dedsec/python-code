4cbf8f412c154ed004b229a8de976a1ece3afd09
Ignacio Zuniga <Izuniga485@gmail.com>
2015-04-24 14:12:56 -0700

    Update project.py
    now it has a way of reading in each character and his id number just need to learn how to add them to a dictionary under python

--- project.py
+++ project.py
@@ -1,21 +1,29 @@
+
 import urllib2
 import json
 import re
 
 def main():
-    response = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/35241849?api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
+    response = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/35241849?&beginIndex=90&endIndex=100&api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
     string = response.read()        #this is a string
     gameList = json.loads(string)   #this is a json object
    
     c = string.count('matchId')
+    got = urllib2.urlopen('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=8fc63904-e5cd-4b76-a555-4729dac804b4')
+    characters = got.read()
+    champion = json.loads(characters)   #this is a json object
+
+    for name in champion["data"].keys():
+        print champion["data"][name]["name"], "=", champion["data"][name]["id"]
+
 
     #theChamps = []
-    print("===Game Data Sample===")
+
     for i in range(0,c):
-        print "Match ID = ", gameList["matches"][i]["matchId"] #the u' annotation is unicode.  you can ignore it
-        print "Champion ID = ", gameList["matches"][i]["participants"][0]["championId"] #the u' annotation is unicode.  you can ignore it
-        print "Win = ", gameList["matches"][i]["participants"][0]["stats"]["winner"], "\n" #the u' annotation is unicode.  you can ignore it
-        
+        print gameList["matches"][i]["matchId"] #the u' annotation is unicode.  you can ignore it
+        print gameList["matches"][i]["participants"][0]["championId"] #the u' annotation is unicode.  you can ignore it
+        print gameList["matches"][i]["participants"][0]["stats"]["winner"] #the u' annotation is unicode.  you can ignore it
+
         #print gameList["matches"][i]["ParticipantStats"][0]["winner"]
         #heChapms.append(gameList["matches"][i]["ParticipantStats"][0]["winner"])
 
