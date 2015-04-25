aeaa1f8a65f5101296272f55f691059e6c78420d
Andrew Ahn <andrewSRahn@gmail.com>
2015-04-25 14:58:04 -0700

    LeagueOfLegends.display() working
    i <3 coffee

--- main.py
+++ main.py
@@ -1,54 +1,83 @@
 '''
 http://www.themeta.gg
 
+Queries Riot API
+https://na.api.pvp.net/observer-mode/rest/featured?
+api_key=e63ca19d-7ce7-4fc7-9b85-35759aab7ec6
+
+Constructs an object: LeagueOfLegends and stores values
 
 '''
 import urllib2
 import json
 
 class LeagueOfLegends():
+    '''
+    self.variables
+    ---------------------------------------------------
+    riotGameId: int
+    riotPlatformId: string (ex. NA1)
+    riotMapId:  int (11 is summoner's rift)
+    riotGameQueueConfigId: int (4 is ranked solo queue)
+    riotParticipants: list of dict{???}
+    riotBannedChampions:  list of dict{???}
+    banned:  list of championIds
+    played:  list of championIds
+    spell:  list of spellIds
+    '''
+
     def __init__(self):
-        print('League object saved')
+        self.riotGameId = int
+        self.riotPlatformId = None
+        self.riotMapId = int
+        self.riotGameQueueConfigId = int
+        self.riotParticipants = []
+        self.riotBannedChampions = []
+        self.banned = []
+        self.spell = []
+        self.played = []
 
-    def parse(self):
+    def parse(self, game):
+        self.riotGameId = game['gameId']
+        self.riotPlatformId = game['platformId']
+        self.riotMapId = game['mapId']
+        self.riotGameQueueConfigId = game['gameQueueConfigId']
+        self.riotParticipants = game['participants']             #list
+        self.riotBannedChampions = game['bannedChampions']       #list
+        #parses the lists
+        self.banned = []
         for champion in self.riotBannedChampions:
-            riotChampionId = champion['championId']
+            self.banned.append(champion['championId'])
+        self.played = []
+        self.spell =[]
+        for participant in self.riotParticipants:
+            if(participant['bot']) == True:
+                break
+            self.played.append(participant['championId'])
+            self.spell.append(participant['spell1Id'])
+            self.spell.append(participant['spell2Id'])
+        #end parse
 
-    def __str__(self):
-        print('-------------------------------------------')
-        print('-------------------------------------------')
+    def display(self):
         print('riotGameId ' + str(self.riotGameId))
         print('riotPlatformId ' + str(self.riotPlatformId))
         print('riotMapId ' + str(self.riotMapId))
         print('riotGameQueueConfigId ' + str(self.riotGameQueueConfigId))
-        print('riotMapId' + str(self.riotMapId))        
-        print('riotPlatformId ' + str(self.riotPlatformId))
-        print('-------------------------------------------')
-
-        print('participants ' + str(self.riotParticipants))
-        print('bannedChampions ' + str(self.riotBannedChampions))
-        print('-------------------------------------------')
+        print('played: ' + str(self.played))
+        print('banned: ' + str(self.banned))
+        print('spell: ' + str(self.spell))
         print('-------------------------------------------')
 
 
 def main():
-    response = urllib2.urlopen('https://na.api.pvp.net/observer-mode/rest/' +
+    r = urllib2.urlopen('https://na.api.pvp.net/observer-mode/rest/' +
                 'featured?api_key=e63ca19d-7ce7-4fc7-9b85-35759aab7ec6')
-    riot_response = response.read()                 #string
-    riot_response = json.loads(riot_response)       #dict 
-    
-    buffer_game = LeagueOfLegends()
-    for game in riot_response['gameList']:
-        buffer_game.riotGameId = game['gameId']
-        buffer_game.riotPlatformId = game['platformId']
-        buffer_game.riotMapId = game['mapId']
-        buffer_game.riotGameQueueConfigId = game['gameQueueConfigId']
-        buffer_game.riotParticipants = game['participants']             #array
-        buffer_game.riotMapId = game['mapId']
-        buffer_game.riotBannedChampions = game['bannedChampions']       #array
-        buffer_game.riotPlatformId = game['platformId']
-        
-        print(buffer_game.__str__())
+    rr = json.loads(r.read()) #dict
+    l = LeagueOfLegends()
+    for game in rr['gameList']:
+        l.parse(game)  #parses the dict
+        l.display()
+        l.store()
 
 if __name__ == '__main__':
     main()
