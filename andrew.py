56446aa95d0da004cb990bb33f74654d4fffd1d8
andrewSRahn <andrewSRahn@gmail.com>
2015-05-21 17:01:49 -0700

    Calculate Global Stats
    added new function that calculates global stats
    also updated docstring

--- andrew.py
+++ andrew.py
@@ -1,10 +1,5 @@
 '''
-Checks if "matchId" already exists in "matchData" collection.
-If it doesn't exist, continue.
 
-Queries "Match" Riot API using specified "matchId" parameter.
-
-Inserts parsed "match" into "matchData" collection
 
 '''
 
@@ -12,13 +7,21 @@ import pymongo
 import urllib2
 import json
 import pprint
+import datetime
 
 def matchData(matchId):
+'''
+This function inserts a parsed (hence skinny) "match" document into 
+"SkinnyMatchData" collection using "matchId" parameter.
+
+Queries using "match-v2.2" webhook located at 
+https://developer.riotgames.com/api/methods#!/967
+'''
     #check database
     client = pymongo.MongoClient()
-    db = client.test_database
-    exists = db.matchData.find({"matchId": matchId })
-    if exists:
+    db = client.LeagueOfLegends
+    cursor = db.SkinnyMatchData.find({"matchId": matchId })
+    if cursor.count() > 0:
     	print("match found, returning without inserting")
     	return
     
@@ -96,17 +99,74 @@ def matchData(matchId):
     match["participants"] = participants
 
     #insert into database
-    db.matchData.insert(match)
-
-
-
+    db.SkinnyMatchData.insert(match)
+    
     #debug printing
-    pp = pprint.PrettyPrinter()
-    pp.pprint(match)
+    # pp = pprint.PrettyPrinter()
+    # pp.pprint(match)
+    print('SkinnyMatchData document entered!')
 
+def calculateGlobalStats():
+	'''
+	This function inserts a document "stat" into "GlobalStats" collection based 
+	upon "SkinnyMatchData" collection.  
 
+	Each "stat" document has a datetime.now() located in stat["currentTime"]
 
+	'''
+	client = pymongo.MongoClient()
+	db = client.LeagueOfLegends
+	cursor = db.SkinnyMatchData.find()
+	stat = {	"matchCount": 0,
+				"participants": 0,
+				"champions": 0,
+				"bans": 0,
+				"masteries": 0,
+				"runes": 0,
+				"spells": 0,
+				"tiers": {
+					"challenger": 0,
+					"diamond": 0,
+					"platinum": 0,
+					"gold": 0,
+					"silver": 0,
+					"bronze": 0
+				},
+				"currentTime": datetime.datetime.now()
+	}
+
+	for match in cursor:
+		stat["matchCount"] += 1
+		for ban in match["bans"]:
+			stat["bans"] += 1
+		for participant in match["participants"]:
+			stat["participants"] += 1
+			if participant["championId"]:
+				stat["champions"] += 1
+			for mastery in participant["masteries"]:
+				stat["masteries"] += 1
+			for rune in participant["runes"]:
+				stat["runes"] += 1
+			for spell in participant["spells"]:
+				stat["spells"] += 1
+			tier = participant["highestAchievedSeasonTier"]
+			if tier:
+				if tier == "CHALLENGER":
+					stat["tiers"]["challenger"] += 1
+				elif tier == "DIAMOND":
+					stat["tiers"]["diamond"] += 1
+				elif tier == "PLATINUM":
+					stat["tiers"]["platinum"] += 1
+				elif tier == "GOLD":
+					stat["tiers"]["gold"] += 1
+				elif tier == "SILVER":
+					stat["tiers"]["silver"] += 1
+				elif tier == "BRONZE":
+					stat["tiers"]["bronze"] += 1
+	db.GlobalStats.insert(stat)
+	print("Global stats updated")
 
 if __name__ == "__main__":
+	calculateGlobalStats()
 	matchData(1721458584)
     
