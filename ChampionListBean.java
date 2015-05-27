1b49372564644605b5ec09ad62aee843203d2ae0
hfsanchez0889 <hfsanchez0889@gmail.com>
2015-05-27 12:30:53 -0700

    Update ChampionListBean.java

--- ChampionListBean.java
+++ ChampionListBean.java
@@ -2,6 +2,7 @@ package model;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.mongodb.BasicDBObject;
@@ -14,17 +15,24 @@ import com.mongodb.Mongo;
 public class ChampionListBean {
 	private List<Champion> champList;
 	private Mongo mongo = null;
+	//private Mongo mongo2 = null;
 	private DB db = null;
+	private DB db2 = null;
 	private DBCollection table = null;
+	private DBCollection table2 = null;
 	
-	@SuppressWarnings("deprecation")
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	public ChampionListBean(){
 		champList = new ArrayList<Champion>();
 		
 		try{
-			mongo = new Mongo("localhost");
-			db = mongo.getDB("local");
-			table = db.getCollection("ChampionWin");
+			mongo = new Mongo("108.225.12.135", 27017);
+			// Pull Champ ID, Image, and Name
+			db = mongo.getDB("ChampionWin");
+			table = db.getCollection("posts");
+			// Pull Champs Wins, and Losses
+			db2 = mongo.getDB("ChampInfo");
+			table2 = db2.getCollection("info");
 			
 		}catch(UnknownHostException e){
 			System.out.println(e.getMessage());
@@ -32,15 +40,36 @@ public class ChampionListBean {
 			System.out.println(e.getMessage());
 		}
 		
+		BasicDBObject search = new BasicDBObject();
+		DBObject row = new BasicDBObject();
+		DBCursor cursor2 = table2.find(search);
+		while(cursor2.hasNext()){
+			row = cursor2.next();
+			Champion newChamp = new Champion((int)row.get("_id"), row.get("image"), (String)row.get("name"));
+			champList.add(newChamp);
+		}
+		
+		
 		BasicDBObject searchQuery = new BasicDBObject();
 		DBObject line = new BasicDBObject();
 		
 		DBCursor cursor = table.find(searchQuery);
+		
+	
 		while(cursor.hasNext()){
 			line = cursor.next();
-			Champion newChamp = new Champion((int)line.get("_id"), (int)line.get("wins"), (int)line.get("lost") );
-			champList.add(newChamp);
+			for(Champion champ: champList){
+				if(champ.getChampionID() == (int)line.get("_id")){
+					champ.setWins((int)line.get("wins"));
+					champ.setLoses((int)line.get("lost"));
+				}
+			}
+			
 		}
+		Collections.sort(this.champList);
+		
+		
+		
 	}
 	
 	public List<Champion> getChampList(){
