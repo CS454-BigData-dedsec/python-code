1bed0011b7b4c660f0dc8a2ab7b65cde7ca72fd0
Andrew Ahn <andrewSRahn@gmail.com>
2015-05-27 23:51:30 -0700

    error handling
    changes made to handle api call rate limit

--- andrew.py
+++ andrew.py
@@ -8,6 +8,7 @@ import urllib2
 import json
 import pprint
 import datetime
+import time
 
 uri = "108.225.12.135"
 
@@ -35,81 +36,89 @@ def matchData(matchId):
 		return
 
 	#api call
-	webhook = ("https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(matchId) + 
-				"?api_key=e63ca19d-7ce7-4fc7-9b85-35759aab7ec6")
-	response = urllib2.urlopen(webhook)
-	response = json.loads(response.read())
+	while True:
+		try:
+			webhook = ("https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(matchId) + 
+						"?api_key=e63ca19d-7ce7-4fc7-9b85-35759aab7ec6")
+			response = urllib2.urlopen(webhook)
+			response = json.loads(response.read())
 
-	#response parsing
-	match = { 	"mapId": response["mapId"],
-				"matchCreation": response["matchCreation"],
-				"matchDuration": response["matchDuration"],
-				"_id": response["matchId"],
-				"matchMode": response["matchMode"],
-				"matchType": response["matchType"],
-				"matchVersion": response["matchVersion"],
-				"platformId": response["platformId"],
-				"queueType": response["queueType"],
-				"region": response["region"],
-				"season": response["season"]
-	}
+			#response parsing
+			match = { 	"mapId": response["mapId"],
+						"matchCreation": response["matchCreation"],
+						"matchDuration": response["matchDuration"],
+						"_id": response["matchId"],
+						"matchMode": response["matchMode"],
+						"matchType": response["matchType"],
+						"matchVersion": response["matchVersion"],
+						"platformId": response["platformId"],
+						"queueType": response["queueType"],
+						"region": response["region"],
+						"season": response["season"]
+			}
 
-	#teams
-	winTeamId = 0
-	bans = []
-	for team in response["teams"]:
-		if team["winner"] == True:
-			winTeamId = team["teamId"]
-		for ban in team["bans"]:
-			bans.append(ban["championId"])
-	match["bans"] = bans
+			#teams
+			winTeamId = 0
+			bans = []
+			for team in response["teams"]:
+				if team["winner"] == True:
+					winTeamId = team["teamId"]
+				for ban in team["bans"]:
+					bans.append(ban["championId"])
+			match["bans"] = bans
 
-	#participants
-	participants = []
-	for p in response["participants"]:
-		current = {	"championId": p["championId"],
-					"highestAchievedSeasonTier": p["highestAchievedSeasonTier"],
-					"masteries": p["masteries"],
-					"runes": p["runes"],
-					"spells": [ p["spell1Id"], p["spell2Id"] ]
-		}
+			#participants
+			participants = []
+			for p in response["participants"]:
+				current = {	"championId": p["championId"],
+							"highestAchievedSeasonTier": p["highestAchievedSeasonTier"],
+							"masteries": p["masteries"],
+							"runes": p["runes"],
+							"spells": [ p["spell1Id"], p["spell2Id"] ]
+				}
 
-		#did participant win?
-		if p["teamId"] == winTeamId:
-			win = True
-		elif p["teamId"] != winTeamId:
-			win = False
-		current["win"] = win
+				#did participant win?
+				if p["teamId"] == winTeamId:
+					win = True
+				elif p["teamId"] != winTeamId:
+					win = False
+				current["win"] = win
 
-		#ParticipantStats
-		stats = p["stats"]
-		if stats["firstBloodAssist"] or stats["firstBloodKill"] is True:
-			firstBlood = True
-		else:
-			firstBlood = False
-		current["firstBlood"] = firstBlood
+				#ParticipantStats
+				stats = p["stats"]
+				if stats["firstBloodAssist"] or stats["firstBloodKill"] is True:
+					firstBlood = True
+				else:
+					firstBlood = False
+				current["firstBlood"] = firstBlood
 
-		if stats["firstTowerAssist"] or stats["firstTowerKill"] is True:
-			firstTower = True
-		else:
-			firstTower = False
-		current["firstTower"] = firstTower
+				if stats["firstTowerAssist"] or stats["firstTowerKill"] is True:
+					firstTower = True
+				else:
+					firstTower = False
+				current["firstTower"] = firstTower
 
-		items = []
-		for x in range(7):
-			items.append(stats["item" + str(x)])
-		current["items"] = items
+				items = []
+				for x in range(7):
+					items.append(stats["item" + str(x)])
+				current["items"] = items
 
-		#ParticipantTimeLine
-		current["lane"] = p["timeline"]["lane"]
-		current["role"] = p["timeline"]["role"]
+				#ParticipantTimeLine
+				current["lane"] = p["timeline"]["lane"]
+				current["role"] = p["timeline"]["role"]
 
-		participants.append(current)
-	match["participants"] = participants
+				participants.append(current)
+			match["participants"] = participants
 
-	#insert into database
-	db.SkinnyMatchData.insert(match)
-
+			#insert into database
+			db.SkinnyMatchData.insert(match)
+			break
+		except urllib2.HTTPError:
+			print("Rate limit reached.  Sleeping for 10 seconds...")
+			time.sleep(10)
+		except KeyError:
+			print("Match " + str(matchId) + " unparsable.")
+			return
 	print('SkinnyMatchData document entered!')
 
 def globalStats():
